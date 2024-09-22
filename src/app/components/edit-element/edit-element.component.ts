625858dcdefad36e6aeb44f5b84d6d0fb0ed0403
Najssky <sokolowski.m98@gmail.com>
2024-09-22 22:19:55 +0200

    Add form validators

--- src/app/components/edit-element/edit-element.component.ts
+++ src/app/components/edit-element/edit-element.component.ts
@@ -2,10 +2,11 @@ import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 
 export interface DialogData {
+  position: number;
   name: string;
   weight: number;
   symbol: string;
@@ -14,30 +15,59 @@ export interface DialogData {
 @Component({
   selector: 'app-edit-element',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './edit-element.component.html',
-  styleUrls: ['./edit-element.component.css']
+  styleUrls: ['./edit-element.component.css'],
 })
-export class EditElementComponent {
+export class EditElementComponent{
   originalData: DialogData;
-
+  editForm: FormGroup;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
-    private dialogRef: MatDialogRef<EditElementComponent>
+    private dialogRef: MatDialogRef<EditElementComponent>,
+    private fb: FormBuilder
   ) {
     this.originalData = { ...data };
+    this.editForm = this.fb.group({
+      name: [data.name, [Validators.required, this.invalidCharsValidator]],
+      weight: [data.weight, [Validators.required]],
+      symbol: [data.symbol, [Validators.required, this.invalidCharsValidator]]
+    });
   }
 
-  onSave(): void {
-    if (
-      this.data.name === this.originalData.name &&
-      this.data.weight === this.originalData.weight &&
-      this.data.symbol === this.originalData.symbol
-    ) {
-      this.dialogRef.close();
-      return;
+  invalidCharsValidator(control: AbstractControl): ValidationErrors | null {
+    const isWhitespace = (control.value || '').trim().length === 0;
+    const hasSpecialChars = /[!@#$%^&*(),.?":{}|<>]/.test(control.value);
+    
+    if (isWhitespace || hasSpecialChars) {
+      return { 'invalidInput': true };
     }
-    this.dialogRef.close(this.data);
+    
+    return null;
+  }
+  onSave(): void {
+    if (this.editForm.valid) {
+      const updatedData = {
+        position: this.originalData['position'],
+        name: this.editForm.value.name,
+        weight: this.editForm.value.weight,
+        symbol: this.editForm.value.symbol
+      };
+      if (
+        updatedData.name === this.originalData.name &&
+        updatedData.weight === this.originalData.weight &&
+        updatedData.symbol === this.originalData.symbol
+      ) {
+        this.dialogRef.close();
+        console.log(this.originalData)
+        return;
+      }
+      this.dialogRef.close(updatedData);
+  
+      console.log(updatedData)
+    } else {
+      alert('Form is invalid');
+    }
   }
 
   onCancel(): void {
